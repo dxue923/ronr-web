8d2e49196542dc6dd04f11e6898d27a7f6bda160
Dave Xue <dxue923@gmail.com>
2025-10-06 18:49:59 -0400

    Improve Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,105 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import "../assets/styles/index.css";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   return (
-    <nav className="navbar">
-      <ul>
-        <li>
-          <Link to="/">Chat</Link>
-        </li>
-        <li>
-          <Link to="/edit-profile">Edit Profile</Link>
-        </li>
-        <li>
-          <Link to="/signin">Sign In</Link>
-        </li>
-      </ul>
-    </nav>
+    <div className="navbar-container">
+      <div id="navbar-menu" className={`navbar-menu ${isOpen ? "open" : ""}`}>
+        <nav className="navbar-links" role="navigation">
+          {/* Create comittee button */}
+          <Link
+            className="navbar-link"
+            to="/create-committee"
+            title="Create Committee"
+            aria-label="Create new committee"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              width="24"
+              height="24"
+            >
+              <path
+                d="M12 2C6.48 2 2 6.48 2 12s4.48 
+                       10 10 10 10-4.48 10-10S17.52 2 
+                       12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z"
+              />
+            </svg>
+          </Link>
+
+          {/* Edit profile button */}
+          <Link className="navbar-link" to="/edit-profile" title="Profile">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              width="24"
+              height="24"
+            >
+              <path
+                d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 
+                       1.79-4 4 1.79 4 4 4zm0 
+                       2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"
+              />
+            </svg>
+          </Link>
+
+          {/* Sign out button */}
+          <Link className="navbar-link" to="/signin" title="Sign Out">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              width="24"
+              height="24"
+            >
+              <path d="M0 0h24v24H0z" fill="none" />
+              <path
+                d="M17 7l-1.41 1.41L18.17 
+                       11H8v2h10.17l-2.58 2.58L17 
+                       17l5-5zM4 5h8V3H4c-1.1 
+                       0-2 .9-2 2v14c0 1.1.9 2 
+                       2 2h8v-2H4V5z"
+              />
+            </svg>
+          </Link>
+        </nav>
+      </div>
+
+      {/* Menu toggle button */}
+      <button
+        id="navbar-toggle"
+        className="navbar-link"
+        title="Actions"
+        type="button"
+        onClick={toggleMenu}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          width="24"
+          height="24"
+        >
+          <path d="M0 0h24v24H0V0z" fill="none" />
+          <path
+            d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 
+                   .9-2 2 .9 2 2 2zm0 2c-1.1 
+                   0-2 .9-2 2s.9 2 2 2 2-.9 
+                   2-2-.9-2-2-2zm0 6c-1.1 
+                   0-2 .9-2 2s.9 2 2 2 2-.9 
+                   2-2-.9-2-2-2z"
+          />
+        </svg>
+      </button>
+    </div>
   );
 }
--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -9,9 +9,17 @@ export default function Discussion() {
         <h3 className="committees">Committees</h3>
         <nav className="committee">
           <ul>
-            <li><a href="#" className="active">Committee A</a></li>
-            <li><a href="#">Committee B</a></li>
-            <li><a href="#">Committee C</a></li>
+            <li>
+              <a href="#" className="active">
+                Committee A
+              </a>
+            </li>
+            <li>
+              <a href="#">Committee B</a>
+            </li>
+            <li>
+              <a href="#">Committee C</a>
+            </li>
           </ul>
         </nav>
       </div>
@@ -34,43 +42,20 @@ export default function Discussion() {
           <button className="chip">Reject Motion</button>
         </section>
         <section className="composer">
-          <button className="plus-btn" aria-label="More options">+</button>
+          <button className="plus-btn" aria-label="More options">
+            +
+          </button>
           <form className="comment-form" onSubmit={(e) => e.preventDefault()}>
             <input className="input" placeholder="Write a comment..." />
-            <button className="submit" type="submit" aria-label="Submit comment">
+            <button
+              className="submit"
+              type="submit"
+              aria-label="Submit comment"
+            >
               <i className="fa fa-arrow-up" aria-hidden="true"></i>
             </button>
           </form>
         </section>
-      </div>
-
-      <div className="site-header--topright">
-        <div id="collapsible-menu" className="collapsible-menu">
-          <nav className="tabs-inline" role="navigation" aria-label="Sections">
-            <a className="icon-link" href="../CreateCommittee/index.html" title="Create Committee" aria-label="Create new committee">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
-                <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z" />
-              </svg>
-            </a>
-            <a className="icon-link" href="../EditProfile/index.html" title="Profile" aria-label="Profile">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
-                <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
-              </svg>
-            </a>
-            <a className="icon-link" href="../Signin/signin.html" title="Sign Out" aria-label="Sign out">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
-                <path d="M0 0h24v24H0z" fill="none" />
-                <path d="M17 7l-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z" />
-              </svg>
-            </a>
-          </nav>
-        </div>
-        <button id="menu-toggle" className="icon-link" title="Actions" aria-label="Open actions menu" type="button">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
-            <path d="M0 0h24v24H0V0z" fill="none" />
-            <path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z" />
-          </svg>
-        </button>
       </div>
 
       <div className="right-main">
