4b6683387101c665b6207548d57d3b3a6d9ef661
nathalieforsythe <nforsythe22@penguinhall.org>
2025-10-27 13:54:51 -0400

    created route to create committee page

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Chat from "../pages/Chat";
 import EditProfile from "../pages/EditProfile";
+import CreateCommittee from "../pages/CreateCommittee";
 import SignIn from "../pages/SignIn";
 import CreateAccount from "../pages/CreateAccount";
 import Home from "../pages/Home";
@@ -19,6 +20,8 @@ export default function App() {
         <Route path="/" element={<Home />} />
 
         <Route path="/discussion" element={<Chat />} />
+
+        <Route path="/create-committee" element={<CreateCommittee />} />
 
         <Route path="/edit-profile" element={<EditProfile />} />
         <Route path="/signin" element={<SignIn />} />
